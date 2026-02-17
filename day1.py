99532df5b624c87790004fb2b37c5d12ffc40a6d
Gowtham <gowthamvicky2526@gmail.com>
2026-02-17 10:16:21 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,94 @@
+# Apple_Fruit=10
+# print(Apple_Fruit)
+
+# # datatypes
+
+# # int,float,str,bool
+
+# # operators
+
+# birth_year=2005
+# future_year=2026
+# age=future_year-birth_year
+# print("Your Age:",age)
+
+# current_age=20
+# maximum_age=60
+# estimated_amount=100
+# total_days_per_year=365
+# total=((maximum_age-current_age)*total_days_per_year)*estimated_amount
+# print(total)
+
+# current_age=20
+# maximum_age=60
+# drinking_water_per_day=3
+# total_days_per_year=365
+# total=((maximum_age-current_age)*total_days_per_year)*drinking_water_per_day
+# print(total)
+
+# trip_distance=10000
+# car_mileage=15
+# petrol_price=101.98
+# fuel_needed=trip_distance/car_mileage
+# total_expense=fuel_needed*petrol_price
+# print(fuel_needed)
+# print(total_expense)
+
+# daily_data=2
+# days_in_month=30
+# monthly_data_usage=daily_data*days_in_month
+# print(monthly_data_usage)
+
+# current_age=20
+# Days_in_a_year=365
+# year_in_days=current_age*Days_in_a_year
+# print(year_in_days)
+
+# original_price=5000
+# discount_percentage=10
+# percentage_to_decimal=discount_percentage/100
+# discount_amt=origina
+# y=int(input("enterl_price*percentage_to_decimal
+# total_amount=original_price-discount_amt
+# print(total_amount)
+
+# x=int(input("enter the number :")) the number :"))
+# if(x==y):
+#     print("X and Y are equal")
+# elif (x>y):
+#     print("X is greater than Y")
+# else:
+#     print("X is less than Y")
+
+# variable= input("Enter the variable:")
+# if (variable == str("a" or "e" or "i" or "o" or "u")):
+#     print("Its an vowel")
+# else:
+#     print("its consonant")
+
+# mark=int(input("Enter your Mark :"))
+# if (mark==100):
+#     print("Grade:S")
+# elif(mark>=90 and mark<=99):
+#     print("Grade:A")
+# elif(mark>=80 and mark<=89):
+#     print("Grade:B")
+# elif(mark>=70 and mark<=79):
+#     print("Grade:C")
+# elif(mark>=60 and mark<=69):
+#     print("Grade:D")
+# elif(mark>=50 and mark<=59):
+#     print("Grade:E")
+# elif(mark<50):
+#     print("Grade:F")
+
+x=float(input("Enter the price of dozen of banana:"))
+y=float(input("price of single banana:"))
+value=y*12
+amt=value/y
+if(x<=value):
+    print("Loss:",amt)
+elif(x>=value):
+    print("profit:",amt)
+elif(x==value):
+    print("There is no loss and profit")
