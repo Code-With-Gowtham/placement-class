f3e882d65a35adf8d65541a8dd607ec943760c2b
Gowtham <gowthamvicky2526@gmail.com>
2026-02-26 16:29:45 +0530

    Add files via upload

---
+++ day10.py
@@ -0,0 +1,94 @@
+#Linked List
+
+#Advantages
+#1)Stored in memory
+#2)Fast access by index
+#3)Must traverse step by step
+#4)Flexible size
+#BASIC STRUCTURE
+#data and address  of the next node
+
+# [10|next]-->[20|next]-->[30|next]-->[40|None]
+
+#Step by step logic
+
+#create a class node
+#Store data
+#Store reference to next node
+
+#create linkedlist class
+#Create hea pointer
+#head stores first node
+
+
+# class Node:
+#     def __init__(self,data):
+#         self.data=data
+#         self.next=None
+# n1=Node(10)
+# n2=Node(20)
+# n3=Node(30)
+# n1.next=n2
+# n2.next=n3
+# n3.next=n1
+# print(n1.next.data)
+# print(n2.next.data)
+# print(n3.next.data)
+
+
+# for _ in range (2):
+#     print("HelloWorld")
+
+
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+
+class LinkedList:
+    def __init__(self):
+        self.head=None
+
+    def insert_begin(self,data):
+        newNode=Node(data)
+        newNode.next=self.head
+        self.head=newNode
+    def insert_end(self,data):
+        endnewNode=Node(data)
+        if self.head is None:
+            self.head=endnewNode
+            return
+        temp=self.head
+        while temp.next:
+            temp=temp.next
+        temp.next=endnewNode
+
+    def insert_pos(self,pos,data):
+        newNode=Node(data)
+        if pos==1:
+            newNode.next=self.head
+            self.head=newNode
+            return
+        temp=self.head
+        for _ in range(pos-2):
+            temp=temp.next
+        newNode.next=temp.next
+        temp.next=newNode
+
+    def display (self):
+        temp=self.head
+        while temp:
+            print(temp.data,end="->")
+            temp=temp.next
+        print("None")
+li=LinkedList()
+li.insert_begin(10)
+li.insert_begin(5)
+li.insert_begin(20)
+li.insert_begin(30)
+li.insert_begin(40)
+li.insert_end(50)
+li.insert_end(40)
+li.insert_begin(92)
+li.insert_pos(4,15)
+li.display()
